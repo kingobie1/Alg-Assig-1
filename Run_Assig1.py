0b4326f37f3dfe6f17d95160f81959d365404e89
Oba Seward-Evans <oba.seward@gmail.com>
2016-09-06 14:41:43 -0400

    added Functions file

--- Run_Assig1.py
+++ Run_Assig1.py
@@ -1,13 +1,15 @@
 """
-Obatola Seward-Evans
+Obatola Seward-Evans, Dimitar Vouldjef, Frank Egan, Himanjal Sharma
 CS 4341 Assignment 1
 September 9th, 2016
 
+This file requires Functions.py which holds the iterative deepending
+and greedy seach functions.
 """
 #!/usr/bin/python
 
 # import the actual functions from Functions.py:
-# from Functions import *
+from Functions import *
 from collections import namedtuple
 
 # Operation structs hold individual tuples such as (+, 2) or (/, 3)
@@ -16,5 +18,5 @@ OperationStruct = namedtuple('operation','value')
 # Array of Operation holds all of the possible OperationStructs
 ArrayOfOperations = []
 
-
+iterativeDeepening(ArrayOfOperations)
 
