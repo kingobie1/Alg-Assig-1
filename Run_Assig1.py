cfb5e7950e07618e12d214a72fdb4d024e09a5dd
fegan104 <fegan104@gmail.com>
2016-09-06 14:43:07 -0400

    added line prints

--- Run_Assig1.py
+++ Run_Assig1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import fileinput
 
 """
 Obatola Seward-Evans
@@ -10,11 +9,22 @@ September 9th, 2016
 method = ""
 #This is our bas number we will operate on
 base = 0
+#This is our target number we will try to find
+target = 0
 #This is the total time we are allowed to execute for
-fp = fileinput.input("test_input1.txt"); 
+time = 0
+
+#Open our file and start reading it with takes care of closing
 with open("test_input1.txt") as file:
     method = file.readline().strip()
     base = file.readline().strip()
+    target = file.readline().strip()
+    time = file.readline().strip()
+
+    for line in file:
+    	print line
 
     print method
     print base
+    print target
+    print time
