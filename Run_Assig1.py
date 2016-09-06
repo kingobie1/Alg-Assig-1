97f9fde57a521186fb6966361511ee9a88e0c74b
fegan104 <fegan104@gmail.com>
2016-09-06 14:36:51 -0400

    read in the first three items of a file

--- Run_Assig1.py
+++ Run_Assig1.py
@@ -1,14 +1,20 @@
+#!/usr/bin/python
+import fileinput
+
 """
 Obatola Seward-Evans
 CS 4341 Assignment 1
 September 9th, 2016
-
 """
-#!/usr/bin/python
+#This variable tells us whetehr we should use a greedy or iterative approach
+method = ""
+#This is our bas number we will operate on
+base = 0
+#This is the total time we are allowed to execute for
+fp = fileinput.input("test_input1.txt"); 
+with open("test_input1.txt") as file:
+    method = file.readline().strip()
+    base = file.readline().strip()
 
-# import the actual functions from Functions.py:
-# from Functions import *
-
-
-
-
+    print method
+    print base
