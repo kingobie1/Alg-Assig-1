9326c93b47f162f98a59a967d70c4dadd7719226
Oba Seward-Evans <oba.seward@gmail.com>
2016-09-06 14:21:07 -0400

    added structure and array

--- Run_Assig1.py
+++ Run_Assig1.py
@@ -8,7 +8,13 @@ September 9th, 2016
 
 # import the actual functions from Functions.py:
 # from Functions import *
+from collections import namedtuple
 
+# Operation structs hold individual tuples such as (+, 2) or (/, 3)
+OperationStruct = namedtuple('operation','value')
+
+# Array of Operation holds all of the possible OperationStructs
+ArrayOfOperations = []
 
 
 
