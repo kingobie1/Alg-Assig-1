1850d357711a6e4fd5034b96d8ca174ebff42208
fegan104 <fegan104@gmail.com>
2016-09-06 22:38:06 -0400

    The iterativeDeepening search function now functions

--- Functions.py
+++ Functions.py
@@ -7,11 +7,40 @@ This file holds the functions used in Run_Assig1.py
 """
 
 # - - - "ITERATIVE DEEPENING" FUNCTION - - -
-def iterativeDeepening( listOfOperations ):
+def iterativeDeepening(start, operations, goal):
     print ("I will do iterative deepening!")
-    return
+    depth = 0
+    solution = None
+    while solution == None:
+        visited = [] # <-- Empty List
+        solution = depthLimited(start, operations, goal, depth, visited)
+        depth = depth + 1
+    return solution
+
+def operateOn(start, operation):
+	if operation.operator == "+":
+		return start + operation.value
+	elif operation.operator == "-":
+		return start - operation.value
+	elif operation.operator == "*":
+		return start * operation.value
+	elif operation.operator == "/":
+		return start / operation.value
+	elif operation.operator == "^":
+		return start ** operation.value
+
+def depthLimited(start, operations, goal, depth, visited):
+    if depth == 0 and start == goal:
+        return start
+    elif depth > 0:
+        for nextOp in operations:
+        	child = operateOn(start, nextOp)
+        	visited = depthLimited(child, operations, goal, depth-1, visited)
+        	if visited != None:
+				return visited
+	return None
 
 # - - - "GREEDY SEARCH" FUNCTION - - -
-def greedySearch( listOfOperations ):
+def greedySearch(listOfOperations):
     print ("I will do a greedy Search!")
     return
