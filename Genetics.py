27bdb37ca3736dd373574d496b18cecab1970858
baby-wpi <vouldjeff@Dimitars-MacBook-Pro.local>
2016-09-14 21:12:17 -0400

    Added crossover

--- Genetics.py
+++ Genetics.py
@@ -1,13 +1,48 @@
 import numpy as np
+from random import randint
 
 class Organism:
 	"""docstring for Organism"""
 	gene = []
-	def __init__(self):
-		self.gene = np.random.randint(5, size=10)
+	def __init__(self, geneSeq=None):
+		if geneSeq == None:
+			self.gene = np.random.randint(5, size=10).tolist()
+		else:
+			self.gene = geneSeq
 
 	def printGene(self):
 		return self.gene
 
+	def length(self):
+		return len(self.gene)
+
+	def getGene(self):
+		return self.gene
+
+	def crossover(self, otherOrganism):
+		primary = None
+		secondary = None
+		if self.length() < otherOrganism.length():
+			primary = self.getGene()
+			secondary = otherOrganism.getGene()
+		else:
+			primary = otherOrganism.getGene()
+			secondary = self.getGene()
+
+		cuttingPoint = randint(0, len(primary) - 1)
+
+		newOrganismGene1 = primary[0:cuttingPoint].extend(secondary[cuttingPoint:(len(secondary) - 1)])
+		newOrganismGene2 = secondary[0:cuttingPoint].extend(primary[cuttingPoint:(len(primary) - 1)])
+
+		return (Organism(newOrganismGene1), Organism(newOrganismGene2))
+
+
 org = Organism()
+org2 = Organism()
 print org.printGene()
+print org2.printGene()
+
+new1, new2 = org.crossover(org2)
+print new1.printGene()
+print new2.printGene()
+
