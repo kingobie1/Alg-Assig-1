a53689e96a6e0ad157a82d3e806256003e13681c
Oba Seward-Evans <oba.seward@gmail.com>
2016-09-14 21:11:53 -0400

    import functions

--- Genetics.py
+++ Genetics.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+# import the actual functions from Functions.py:
+import Functions
+
 class Organism:
 	"""docstring for Organism"""
 	gene = []
@@ -16,10 +19,20 @@ class Organism:
 		for index in self.gene:
 			# Todo: listOfOperations requires a masterlist of all possible operations.
 			operation = listOfOperations[index]
-			temp = operateOn(total, operation)
+			temp = Functions.operateOn(total, operation)
 			total = temp
 
 		return total
+
+	def getFitness(self, desiredGoal):
+		goal = desiredGoal
+		total = 0
+		numOps = 0
+
+		total = self.operate
+		difference = diff
+
+
 			
 
 org = Organism()
