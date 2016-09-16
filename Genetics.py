63a4b1434a01e0c4ab6336d8d0f5c240e24f3e41
fegan104 <fegan104@gmail.com>
2016-09-15 22:56:13 -0400

    added some stuff from upstream

--- Genetics.py
+++ Genetics.py
@@ -4,40 +4,61 @@ from random import randint
 # import the actual functions from Functions.py:
 import Functions
 
+NUMOPS = 5
+
 class Organism:
 	"""docstring for Organism"""
-	gene = []
-	def __init__(self, geneSeq=None):
+	#This is an encoding for the solution into an organism
+	#each number is an index into the operators we read in.
+	chromosome = []
+	#The number of operators that we read in, it's used to
+	# encode and mutate organisms
+	numOps = 0
+
+	def __init__(self, numOps=0, geneSeq=None):
+		self.numOps = numOps
 		if geneSeq == None:
-			self.gene = np.random.randint(5, size=10).tolist()
+			self.chromosome = np.random.randint(5, size=10).tolist()
 		else:
-			self.gene = geneSeq
+			self.chromosome = geneSeq
+		
+	def getChromosome(self):
+		return self.chromosome
 
-	def printGene(self):
-		return self.gene
+	def getLen(self):
+		return len(self.chromosome)
 
-	def length(self):
-		return len(self.gene)
-
-	def getGene(self):
-		return self.gene
+	def mutate(self):
+		mType = random.randint(1, 3)
+		if mType == 1:
+			#We are adding a gene as an encoded index
+			op = random.randint(0, self.numOps)
+			self.chromosome.append(op)
+		elif mType == 2:
+			#We are removing genes
+			i = random.randint(1, len(self.chromosome))
+			self.chromosome.pop(i)
+		elif mType == 3:
+			#We are modigying a gene
+			i = random.randint(1, len(self.chromosome))
+			self.chromosome[i] = random.randint(1, self.numOps)
 
 	def crossover(self, otherOrganism):
 		primary = None
 		secondary = None
-		if self.length() < otherOrganism.length():
-			primary = self.getGene()
-			secondary = otherOrganism.getGene()
+		if self.getLen() < otherOrganism.getLen():
+			primary = self.getChromosome()
+			secondary = otherOrganism.getChromosome()
 		else:
-			primary = otherOrganism.getGene()
-			secondary = self.getGene()
+			primary = otherOrganism.getChromosome()
+			secondary = self.getChromosome()
 
 		cuttingPoint = randint(0, len(primary) - 1)
 
 		newOrganismGene1 = primary[0:cuttingPoint].extend(secondary[cuttingPoint:(len(secondary) - 1)])
 		newOrganismGene2 = secondary[0:cuttingPoint].extend(primary[cuttingPoint:(len(primary) - 1)])
 
-		return (Organism(newOrganismGene1), Organism(newOrganismGene2))
+		return (Organism(geneSeq=newOrganismGene1), Organism(geneSeq=newOrganismGene2))
 
 	def operate(self, start):
 		total = start
@@ -50,11 +71,11 @@ class Organism:
 
 		return total
 
-org = Organism()
-org2 = Organism()
-print org.printGene()
-print org2.printGene()
+org = Organism(numOps=NUMOPS)
+org2 = Organism(numOps=NUMOPS)
+print org.getChromosome()
+print org2.getChromosome()
 
 new1, new2 = org.crossover(org2)
-print new1.printGene()
-print new2.printGene()
+print new1.getChromosome()
+print new2.getChromosome()
