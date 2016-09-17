064ef9af1c5fe19b4c10af1bb34057ecc031d2f8
Oba Seward-Evans <oba.seward@gmail.com>
2016-09-17 17:31:14 -0400

    new function getMutatedPopulation

--- Genetics.py
+++ Genetics.py
@@ -12,7 +12,7 @@ as organisms that will be bred and selected fo rthat find optial solutions
 NUMOPS = 5
 INITIAL_ORGANISM_SIZE = 5
 INITIAL_POPULATION_SIZE = 10
-MUTATEPROB = 0.25
+MUTATEPROB = 0.01
 
 def geneticSearch(start, operations, goal, max_exec):
 	"""
@@ -44,22 +44,22 @@ def geneticSearch(start, operations, goal, max_exec):
 	print ".."
 	print "... crossing over ..."
 	print ".."
-	print "TWO_FITTEST " + str(map(lambda o: o.getFitness(start, operations, goal), twoFittestOrganism))
 
-	# TODO: crossover the two fittest organisms
+	# crossover the two fittest organisms
 	crossedOverOrganisms = []
 	organisms1, organisms2 = twoFittestOrganism[0].crossover(twoFittestOrganism[1])
 	crossedOverOrganisms.append(organisms1)
 	crossedOverOrganisms.append(organisms2)
-
 	print "two organisms created by crossover: "
 	print map(lambda o: o.getChromosome(), crossedOverOrganisms)
 	print
 
 
-	# TODO: mutate the product of the crossover (2 organisms)
-	map(lambda o: o.mutate(MUTATEPROB), twoFittestOrganism)
-	print "TWO_FITTEST " + str(map(lambda o: o.getFitness(start, operations, goal), twoFittestOrganism))
+	# mutate the product of the crossover (2 organisms)
+	population = getMutatedPopulation(twoFittestOrganism, INITIAL_POPULATION_SIZE)
+	# print "mutated population: " + str(map(lambda o: o.getChromosome(), population))
+
+	# print "mutated organisms: " + str(map(lambda o: o.getChromosome(), population))
 
 	# TODO: population = the collection of mutated organisms
 
@@ -68,22 +68,52 @@ def geneticSearch(start, operations, goal, max_exec):
 	# print map(lambda o: o.getChromosome(), populate(10, len(operations)))
 	return ([[4, operations[0], 11]], 0.5, 5, 3)
 
+
 def populate(initSize, numOps):
 	population = []
 	for i in xrange(initSize):
 		population.append(Organism(numOps))
 	return population
 
+
 # Utility function for fitness
 def fitnessUtility(lengthOfOrganism):
 	# penalize longer organisms
 	return lengthOfOrganism * 0.1
+
 
 # function that returns a new population of the organisms with 
 # the best fitness from the given population
 def bestOfPopulation(start, operations, goal, population):
 	sortedPopulation =  sorted(population,key =lambda o: o.getFitness(start, operations, goal))
 	return sortedPopulation[0:2]
+
+
+# returns a new population of mutated organisms
+def getMutatedPopulation(twoFittestOrganism, populationSize):
+	mutatedPopulation = []
+
+	# create population of size, populationSize
+	for x in xrange(0, populationSize / 2):
+		mutatedPopulation.extend(twoFittestOrganism)
+
+	# mutate each organism in population
+	for organism in mutatedPopulation:
+		organism.mutate(MUTATEPROB)
+		print organism.getChromosome()
+
+	# for x in xrange(0,populationSize):
+	# 	if x % 2 == 0:
+	# 		newOrganism = twoFittestOrganism[0]
+	# 	else:
+	# 		newOrganism = twoFittestOrganism[1]
+
+	# 	newOrganism.mutate(MUTATEPROB)
+	# 	print newOrganism.getChromosome()
+	# 	mutatedPopulation.append(newOrganism)
+
+	return mutatedPopulation
+
 
 class Organism:
 	"""An Organism represents an encoding of a solution within it's chromosome"""
@@ -108,44 +138,44 @@ class Organism:
 	def getLen(self):
 		return len(self.chromosome)
 
+	# Defines the organisms fitness given the operations and starting base number.
+	# Args:
+	# 	base (float): The starting number we read.
+	# 	ops (OperationStruct[]): An array of operations we read form config file.
+	# 	goal (float): The desired goal of base after operations.
+	# Returns:
+	# 	How close the organism is to the desired value once decoded.
 	def getFitness(self, base, ops, goal):
-		"""
-		Defines the organisms fitness given the operations and starting base number.
-		Args:
-			base (float): The starting number we read.
-			ops (OperationStruct[]): An array of operations we read form config file.
-			goal (float): The desired goal of base after operations.
-		Returns:
-			How close the organism is to the desired value once decoded.
-		"""
 
 		product = self.operate(base, ops)
 		difference = Functions.diff(product, goal)
 		fitness = difference + fitnessUtility(self.getLen())
 		return fitness
 
+	
+	# Mutates the organism either by adding, subtracting or modifyinh a gene.
+	# Args:
+	# 	prob (float): The probability that we will actually mutate. Values of 1 is certain 0 is never.
 	def mutate(self, prob=1):
-		"""
-		Mutates the organism either by adding, subtracting or modifyinh a gene.
-
-		Args:
-			prob (float): The probability that we will actually mutate. Values of 1 is certain 0 is never.
-		"""
-		#Should we mutate?
+		# Should we mutate?
 		if random.random() < prob:
 			return 
-		#Do mutation
+
+		# Do mutation
 		mType = random.randint(1, 3)
+
+		print mType
+
 		if mType == 1:
-			#We are adding a gene as an encoded index
+			# We are adding a gene as an encoded index
 			op = random.randint(0, self.numOps-1)
 			self.chromosome.append(op)
 		elif mType == 2:
-			#We are removing genes
+			# We are removing genes
 			i = random.randint(0, len(self.chromosome)-1)
 			self.chromosome.pop(i)
 		elif mType == 3:
-			#We are modigying a gene
+			# We are modigying a gene
 			i = random.randint(0, len(self.chromosome)-1)
 			self.chromosome[i] = random.randint(0, self.numOps-1)
 
@@ -167,25 +197,18 @@ class Organism:
 		sectionB1 = secondary[0:cuttingPoint]
 		sectionB2 = primary[cuttingPoint:(len(primary))]
 
-		# newOrganismGene1 = sectionA1.extend(sectionA2)
-		# newOrganismGene2 = sectionB1.extend(sectionB2)
-
 		newOrganismGene1 = sectionA1 +sectionA2
 		newOrganismGene2 = sectionB1 + sectionB2
 
-		# newOrganismGene1 = primary[0:cuttingPoint].extend(secondary[cuttingPoint:(len(secondary) - 1)])
-		# newOrganismGene2 = secondary[0:cuttingPoint].extend(primary[cuttingPoint:(len(primary) - 1)])
-
 		return (Organism(self.numOps, geneSeq=newOrganismGene1), Organism(self.numOps, geneSeq=newOrganismGene2))
 
-	def operate(self, base, ops):
-		""" 
-		Goes through each encoded operation in the chromosome and applies that to the base.
+	# Goes through each encoded operation in the chromosome and applies that to the base.
 
-		Args:
-			start (int): The base number form our config file.
-			ops (OperationStruct[]): An array of operations we read from config file.
-		"""
+	# Args:
+	# 	start (int): The base number form our config file.
+	# 	ops (OperationStruct[]): An array of operations we read from config file.
+	def operate(self, base, ops):
+
 		total = base
 		for index in self.chromosome:
 			if index >= len(ops):
@@ -195,17 +218,9 @@ class Organism:
 			total = temp
 		return total
 
-# print map(lambda o: o.getChromosome(), populate(10, len(operations)))
-# org = Organism(NUMOPS)
-# print org.getChromosome()
-# org.mutate()
-# print org.getChromosome()
 
-# org = Organism(numOps=len(operations))
-# org2 = Organism(numOps=len(operations))
-# print org.getChromosome()
-# print org2.getChromosome()
 
-# new1, new2 = org.crossover(org2)
-# print new1.getChromosome()
-# print new2.getChromosome()
+
+
+
+
